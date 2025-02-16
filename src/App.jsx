32fd6407415627506bb1440f3beeb6ad60148be3
mukesh phulwani <mukeshphulwani66@gmail.com>
2025-02-16 11:43:36 +0530

    addd new feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Github Actions</h1>
+      <h1>CI/CD using Github Actions</h1>
       <h2>For beginners v2</h2>
       <h2>With 2 projects</h2>
     </>
